11f4f0a84fb8db1e7a8c7cead0178209f54bbf44
duduskaaa <ergalikhatep@gmail.com>
2025-11-13 17:08:43 +0500

    add (TaskService): find all tasks owned one user

---
+++ src/main/java/com/khatep/tasktracker/models/dto/UserResponseDto.java
@@ -0,0 +1,11 @@
+package com.khatep.tasktracker.models.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResponseDto {
+    private String name;
+    private String email;
+}
