4688fe0ad3453073748941a555d9442b6665c040
duduskaaa <ergalikhatep@gmail.com>
2025-11-16 22:40:26 +0500

    add: Exception handle methods to one GlobalExceptionHandler

---
+++ src/main/java/com/khatep/tasktracker/exceptions/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.khatep.tasktracker.exceptions.handlers;
+
+import com.khatep.tasktracker.exceptions.exceptions.EmailIsAlreadyExists;
+import com.khatep.tasktracker.exceptions.exceptions.PasswordIsNotStrength;
+import com.khatep.tasktracker.exceptions.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    protected ResponseEntity<Map<String, Object>> buildErrorResponse(HttpStatus status, String error, String message) {
+        return ResponseEntity.status(status).body(
+                Map.of(
+                "timestamp", LocalDateTime.now(),
+                "status", status.value(),
+                "error", error,
+                "message", message
+        ));
+    }
+
+    @ExceptionHandler(EmailIsAlreadyExists.class)
+    public ResponseEntity<Map<String, Object>> handleEmailAlreadyExists(EmailIsAlreadyExists ex) {
+        return buildErrorResponse(HttpStatus.CONFLICT, "Email is already exist", ex.getMessage());
+    }
+
+    @ExceptionHandler(PasswordIsNotStrength.class)
+    public ResponseEntity<Map<String, Object>> handlePasswordIsNotStrength(PasswordIsNotStrength ex) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, "Password is strength", ex.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleUserNotFoundException(UserNotFoundException ex) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, "User not found", ex.getMessage());
+    }
+}
