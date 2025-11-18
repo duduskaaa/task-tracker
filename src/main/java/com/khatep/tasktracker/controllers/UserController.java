bc0fce72f3ce25a797417e0ec04cce2338c2afc4
duduskaaa <ergalikhatep@gmail.com>
2025-11-18 18:31:05 +0500

    delete UserController: Unnecessary imports

--- src/main/java/com/khatep/tasktracker/controllers/UserController.java
+++ src/main/java/com/khatep/tasktracker/controllers/UserController.java
@@ -2,11 +2,13 @@ package com.khatep.tasktracker.controllers;
 
 import com.khatep.tasktracker.models.dto.responses.UserResponseDto;
 import com.khatep.tasktracker.services.UserService;
-import com.khatep.tasktracker.services.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
