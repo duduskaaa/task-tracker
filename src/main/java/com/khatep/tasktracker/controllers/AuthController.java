bc54f9751e8776cefd9044cd6b2cdbf92fba6592
duduskaaa <ergalikhatep@gmail.com>
2025-11-18 18:43:44 +0500

    add AuthController: Lombok method for final Constructors

--- src/main/java/com/khatep/tasktracker/controllers/AuthController.java
+++ src/main/java/com/khatep/tasktracker/controllers/AuthController.java
@@ -3,6 +3,7 @@ package com.khatep.tasktracker.controllers;
 import com.khatep.tasktracker.models.dto.requests.UserRequestDto;
 import com.khatep.tasktracker.models.dto.responses.UserResponseDto;
 import com.khatep.tasktracker.services.UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +11,9 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
-
-    public AuthController(UserService userService) {
-        this.userService = userService;
-    }
 
     @PostMapping("/register")
     public ResponseEntity<Void> register(@RequestBody UserRequestDto userRequestDto) {
